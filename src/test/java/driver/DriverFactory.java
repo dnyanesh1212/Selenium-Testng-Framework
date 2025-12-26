66871e828e150a8cecba263f6c736f4f9f2c9273
dnyanesh1212 <dnyana121297@gmail.com>
2025-12-26 14:13:57 +0530

    Add DriverFactory with multi-browser support and options and ThreadLocal DriverManager

--- src/test/java/driver/DriverFactory.java
+++ src/test/java/driver/DriverFactory.java
@@ -1,4 +1,78 @@
 package driver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class DriverFactory {
+
+    private DriverFactory(){}
+
+    public static WebDriver createDriver(String browser){
+
+        WebDriver driver = null;
+        switch (browser.toLowerCase()){
+            case "chrome":
+                driver = createChromeDriver();
+                break;
+            case "firefox":
+                driver = createFirefoxDriver();
+                break;
+            case "edge":
+                driver = createEdgeDriver();
+                        break;
+
+            default:
+                System.out.println("Browser is not valid..!!");
+
+        }
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    private static WebDriver createChromeDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.setAcceptInsecureCerts(true);
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-geolocation");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--start-maximized");
+
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("profile.default_content_setting_values.notifications", 2);
+        prefs.put("profile.default_content_setting_values.geolocation", 2);
+        prefs.put("profile.default_content_setting_values.media_stream_camera", 2);
+        prefs.put("profile.default_content_setting_values.media_stream_mic", 2);
+        options.setExperimentalOption("prefs", prefs);
+
+        return new ChromeDriver(options);
+    }
+
+    private static WebDriver createFirefoxDriver() {
+        FirefoxOptions options = new FirefoxOptions();
+        options.setAcceptInsecureCerts(true);
+
+        options.addPreference("dom.webnotifications.enabled", false);
+        options.addPreference("media.navigator.permission.disabled", true);
+        options.addPreference("geo.enabled", false);
+
+        return new FirefoxDriver(options);
+    }
+
+    private static WebDriver createEdgeDriver() {
+        EdgeOptions options = new EdgeOptions();
+        options.setAcceptInsecureCerts(true);
+        options.addArguments("--disable-notifications");
+        options.addArguments("--start-maximized");
+
+        return new EdgeDriver(options);
+    }
 }
