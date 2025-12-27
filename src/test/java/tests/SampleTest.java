7704f363d461f85e4e08e34535ab6a1968a6817c
dnyanesh1212 <dnyana121297@gmail.com>
2025-12-27 16:43:19 +0530

    Add configuration management with environment support

--- src/test/java/tests/SampleTest.java
+++ src/test/java/tests/SampleTest.java
@@ -9,10 +9,11 @@ import org.testng.annotations.Test;
 public class SampleTest extends BaseTest {
 
     @Test(description = "First sample test")
-    public void sampleSmokeTest() {
+    public void sampleSmokeTest() throws InterruptedException {
 
         WebDriver driver = DriverManager.getDriver();
         System.out.println("Sample Smoke Test executed");
+        Thread.sleep(5000);
         Assert.assertTrue(true);
     }
 }
