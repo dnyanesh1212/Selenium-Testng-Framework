e78993be83dafe88fa4124bbd953d1368e7fbd34
dnyanesh1212 <dnyana121297@gmail.com>
2025-12-27 21:40:15 +0530

    Added WindowUtils for window and tab handling

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -2,16 +2,19 @@ package pages;
 
 import utils.ElementActions;
 import utils.MouseActions;
+import utils.WindowUtils;
 
 public class BasePage {
 
     protected ElementActions elementActions;
     protected MouseActions mouseActions;
+    protected WindowUtils windowUtils;
 
     public BasePage() {
 
         this.elementActions = new ElementActions();
         this.mouseActions = new MouseActions();
+        this.windowUtils = new WindowUtils();
 
     }
 }
