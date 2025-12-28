c34274ef2eeb28a5f77a781bdb5821dce39e515c
dnyanesh1212 <dnyana121297@gmail.com>
2025-12-28 14:34:15 +0530

    Integrate ExtentReports with TestNG listener

--- src/test/java/listeners/TestListener.java
+++ src/test/java/listeners/TestListener.java
@@ -1,20 +1,43 @@
 package listeners;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import reporter.ExtentManager;
+import reporter.ExtentTestManager;
 import utils.Screenshotutils;
+
+import java.io.File;
 
 public class TestListener implements ITestListener {
 
     @Override
     public void onTestStart(ITestResult result) {
 
+        String className = result.getTestClass().getRealClass().getSimpleName();
+        String methodName = result.getMethod().getMethodName();
+
+        String testFolderName = className + "_" + methodName;
+
+        String reportPath = System.getProperty("user.dir")
+                            + "/reports/"
+                            + testFolderName
+                            + "/report.html";
+
+        ExtentReports extent = ExtentManager.getInstance(reportPath);
+
+        ExtentTest test = extent.createTest(methodName, result.getMethod().getDescription());
+
+        ExtentTestManager.setTest(test);
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
 
+        ExtentTestManager.getTest().pass("Test passed successfully..!!");
     }
 
     @Override
@@ -24,13 +47,16 @@ public class TestListener implements ITestListener {
         String methodName = result.getMethod().getMethodName();
 
         String testFolderName = className + "_" + methodName;
-
+        String screenShotPath = null;
         try{
-            String screenShotPath = Screenshotutils.takeScreenshot(testFolderName);
-            System.out.println("Screenshot saved at : " + screenShotPath);
-        } catch (Exception e) {
+            screenShotPath = Screenshotutils.takeScreenshot(testFolderName);
+        }
+        catch (Exception e) {
             throw new RuntimeException("Failed to capture screenshot: " + e.getMessage());
         }
+
+        String relativePath = "screenshots/" + new File(screenShotPath).getName();
+        ExtentTestManager.getTest().fail(result.getThrowable(), MediaEntityBuilder.createScreenCaptureFromPath(relativePath).build());
     }
 
     @Override
@@ -45,6 +71,6 @@ public class TestListener implements ITestListener {
 
     @Override
     public void onFinish(ITestContext context) {
-
+        ExtentManager.flushReports();
     }
 }
